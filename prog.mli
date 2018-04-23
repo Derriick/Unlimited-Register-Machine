f5608bbb59c2e4e12e9831f9f27d1c99384000b3
Derriick <derrick.ensiie@yahoo.com>
2018-04-23 17:41:16 +0200

    ex1 + ex2

---
+++ prog.mli
@@ -0,0 +1,21 @@
+type instruction =
+	| Reset of int
+	| Incr of int
+	| Set of int * int
+	| Jump of int * int * int
+
+type prog = instruction array
+type reg_array = int array
+
+exception Memory_exhausted
+exception Segmentation_fault
+exception Resources_exhausted
+
+val max_reg : int
+val max_steps : int
+
+val print_reg_array : reg_array -> int -> unit
+val print_prog : prog -> unit
+val rho : prog -> int
+val normalize : prog -> prog
+val run_program : reg_array -> prog -> int
